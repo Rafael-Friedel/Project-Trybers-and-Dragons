759746aa9639c237754a56e87f584bc7388fda59
Rafaelindo <RafafriedelDev@outlook.com>
2022-08-09 16:48:08 -0300

    feat: class Warrior

--- src/Archetypes/Warrior.ts
+++ src/Archetypes/Warrior.ts
@@ -1,3 +1,19 @@
-class Warrior {}
+import { EnergyType } from '../Energy';
+import Archetype from './Archetype';
+
+class Warrior extends Archetype {
+  private _energyType: EnergyType = 'stamina';
+  private static instances = 0;
+  constructor(name: string) {
+    super(name);
+    Warrior.instances += 1;
+  }
+
+  static createdArchetypeInstances = (): number => this.instances;
+
+  get energyType(): EnergyType {
+    return this._energyType;
+  }
+}
 
 export default Warrior;
