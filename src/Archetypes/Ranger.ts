8049ca9df972c9926831545046cf2837e5a17344
Rafaelindo <RafafriedelDev@outlook.com>
2022-08-09 16:47:59 -0300

    feat: class Rangeer

--- src/Archetypes/Ranger.ts
+++ src/Archetypes/Ranger.ts
@@ -1,3 +1,19 @@
-class Ranger {}
+import { EnergyType } from '../Energy';
+import Archetype from './Archetype';
+
+class Ranger extends Archetype {
+  private _energyType: EnergyType = 'stamina';
+  private static instances = 0;
+  constructor(name: string) {
+    super(name);
+    Ranger.instances += 1;
+  }
+
+  static createdArchetypeInstances = (): number => this.instances;
+
+  get energyType(): EnergyType {
+    return this._energyType;
+  }
+}
 
 export default Ranger;
