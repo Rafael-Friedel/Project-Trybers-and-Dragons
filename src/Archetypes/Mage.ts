4fe5e59c358ee36c4d46ab5ae9987117fb340f42
Rafaelindo <RafafriedelDev@outlook.com>
2022-08-09 16:47:39 -0300

    feat: class mage

--- src/Archetypes/Mage.ts
+++ src/Archetypes/Mage.ts
@@ -1,3 +1,19 @@
-class Mage {}
+import { EnergyType } from '../Energy';
+import Archetype from './Archetype';
+
+class Mage extends Archetype {
+  private _energyType: EnergyType = 'mana';
+  private static instances = 0;
+  constructor(name: string) {
+    super(name);
+    Mage.instances += 1;
+  }
+
+  static createdArchetypeInstances = (): number => this.instances;
+
+  get energyType(): EnergyType {
+    return this._energyType;
+  }
+}
 
 export default Mage;
