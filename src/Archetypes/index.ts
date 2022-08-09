3b486c315a5987d855a082c643daa8888e8aefcf
Rafaelindo <RafafriedelDev@outlook.com>
2022-08-09 16:49:40 -0300

    feat: importando classes no index

--- src/Archetypes/index.ts
+++ src/Archetypes/index.ts
@@ -1,3 +1,9 @@
 import Archetype from './Archetype';
+import Warrior from './Warrior';
+import Mage from './Mage';
+import Ranger from './Ranger';
+import Necromancer from './Necromancer';
 
 export default Archetype;
+
+export { Warrior, Mage, Ranger, Necromancer };
