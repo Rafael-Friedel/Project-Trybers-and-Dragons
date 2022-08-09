44228159be53786b6c089c2b3350f0107593b87f
Rafaelindo <RafafriedelDev@outlook.com>
2022-08-09 16:47:50 -0300

    feat: class Necromancer

--- src/Archetypes/Necromancer.ts
+++ src/Archetypes/Necromancer.ts
@@ -1,3 +1,19 @@
-class Necromancer {}
+import { EnergyType } from '../Energy';
+import Archetype from './Archetype';
+
+class Necromancer extends Archetype {
+  private _energyType: EnergyType = 'mana';
+  private static instances = 0;
+  constructor(name: string) {
+    super(name);
+    Necromancer.instances += 1;
+  }
+
+  static createdArchetypeInstances = (): number => this.instances;
+
+  get energyType(): EnergyType {
+    return this._energyType;
+  }
+}
 
 export default Necromancer;
